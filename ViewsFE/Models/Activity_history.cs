d2ece897b3633151f4f28dc645127844170f762c
Đức Chuẩn Nguyễn <nguyenducchuan2004@gmail.com>
2024-10-17 15:14:32 +0700

    fix lỗi meger

--- ViewsFE/Models/Activity_history.cs
+++ ViewsFE/Models/Activity_history.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ViewsFE.Models
 {
     public class Activity_history
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string Log_name { get; set; }
         public string Descripcion { get; set; }
         public string Subject_type { get; set; }
+        public DateTime Time { get; set; } = DateTime.UtcNow;
         public string AccountId { get; set; }
-        public virtual Account? Account { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual Account Account { get; set; }
 
     }
 }
