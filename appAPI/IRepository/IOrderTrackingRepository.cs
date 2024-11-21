83e0c8eb7c87801f0f331de1ee540a376ac03cd9
XuanDung09 <xdug94@gmail.com>
2024-11-21 21:53:15 +0700

    oview ordershipping and vieww ordercounter

---
+++ appAPI/IRepository/IOrderTrackingRepository.cs
@@ -0,0 +1,14 @@
+﻿using appAPI.Models;
+
+namespace appAPI.IRepository
+{
+    public interface IOrderTrackingRepository
+    {
+        public Task<List<order_trackings>> GetAll();
+        public Task<order_trackings> GetById(long id);
+        public Task<order_trackings> GetByOrderId(long idOrder);
+        public Task Create(order_trackings address);
+        public Task Update(order_trackings address, long id);
+        public Task Delete(long id);
+    }
+}
