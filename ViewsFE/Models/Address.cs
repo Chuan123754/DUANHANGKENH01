5d18b7bc5c6128afd15e094e0e6d28af2875e95c
XuanDung09 <xdug94@gmail.com>
2024-12-06 17:03:34 +0700

    update orderaddress

--- ViewsFE/Models/Address.cs
+++ ViewsFE/Models/Address.cs
@@ -9,6 +9,8 @@ namespace ViewsFE.Models
         [Key]
         public long Id { get; set; }
         public long? User_Id { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; } // user không đăng nhập thì nhập email để theo dõi đơn 
         public string? Name { get; set; } // tên địa chỉ ( mặc định , địa chỉ 1 2 3 )
         public string? Street { get; set; } // địa chỉ cụ thể 
         public string? Ward_commune { get; set; }
@@ -19,5 +21,6 @@ namespace ViewsFE.Models
         public string? Status { get; set; }
         [ForeignKey("User_Id")]
         public virtual Users? User { get; set; }
+        public virtual Orders? Order { get; set; }
     }
 }
