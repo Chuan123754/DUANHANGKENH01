5a15c3d903fcc42e3488a59d91bcd24ff2ffc9c8
XuanDung09 <xdug94@gmail.com>
2024-12-26 02:47:27 +0700

    lọc + tìm kiếm + gen biến thể sản phẩm

---
+++ appAPI/Migrations/20241222183039_123ok.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appAPI.Migrations
+{
+    public partial class _123ok : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DistrictId",
+                table: "Address",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WardCode",
+                table: "Address",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DistrictId",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "WardCode",
+                table: "Address");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
